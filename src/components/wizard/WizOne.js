c9768a9b693db254bc12c851f01068dba766039a
Thomas Lowry <minilao94@yahoo.com>
2017-09-29 16:13:37 -0600

    editing wizard

--- src/components/wizard/WizOne.js
+++ src/components/wizard/WizOne.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 
 export default class WizOne extends Component {
     render() {
+        let { setProperty, setDescription, next } = this.props
         return (
-            <div className='center_piece'>
-                <div className='form'>
-                    One
-                </div>
+            <div className='WizOne'>
+                <h2>Property Name</h2>
+                <input onChange={e => setProperty(e.target.value)} />
+                <h2>Property Description</h2>
+                <input onChange={e => setDescription(e.target.value)} />
+                <button onClick={next} >next</button>
             </div>
         )
     }
--- src/components/wizard/Wizard.js
+++ src/components/wizard/Wizard.js
@@ -1,13 +1,161 @@
 import React, { Component } from 'react';
+import WizOne from './WizOne';
+import WizTwo from './WizTwo';
+import WizThree from './WizThree';
+import WizFour from './WizFour';
+import WizFive from './WizFive';
 
 export default class Wizard extends Component {
+
+    constructor() {
+        super()
+        this.state = {
+            property: '',
+            description: '',
+            address: '',
+            city: '',
+            state: '',
+            zip: '',
+            imgurl: '',
+            mortgage: '',
+            loan: '',
+            rent: '',
+            wizard: 1
+        }
+        this.next = this.next.bind(this);
+        this.setProperty = this.setProperty.bind(this);
+        this.setDescription = this.setDescription.bind(this);
+        this.setAddress = this.setAddress.bind(this);
+        this.setCity = this.setCity.bind(this);
+        this.updateState = this.updateState.bind(this);
+        this.setZip = this.setZip.bind(this);
+        this.setImgurl = this.setImgurl.bind(this);
+        this.setMortgage = this.setMortgage.bind(this);
+        this.setLoan = this.setLoan.bind(this);
+        this.setRent = this.setRent.bind(this);
+        this.previous = this.previous.bind(this);
+        this.complete = this.complete.bind(this);
+    }
+
+    previous() {
+        this.setState({
+            wizard: this.state.wizard - 1
+        })
+    }
+
+    next() {
+        this.setState({
+            wizard: this.state.wizard + 1
+        })
+    }
+
+    complete() {
+        console.log('completed')
+    }
+
+    setProperty(val) {
+        this.setState({
+            property: val
+        })
+    }
+
+    setDescription(val) {
+        this.setState({
+            description: val
+        })
+    }
+    setAddress(val) {
+        this.setState({
+            address: val
+        })
+    }
+    setCity(val) {
+        this.setState({
+            city: val
+        })
+    }
+    updateState(val) {
+        this.setState({
+            state: val
+        })
+    }
+    setZip(val) {
+        this.setState({
+            zip: val
+        })
+    }
+    setImgurl(val) {
+        this.setState({
+            imgurl: val
+        })
+    }
+    setMortgage(val) {
+        this.setState({
+            mortgage: val
+        })
+    }
+    setLoan(val) {
+        this.setState({
+            loan: val
+        })
+    }
+    setRent(val) {
+        this.setState({
+            rent: val
+        })
+    }
+
     render() {
-        return (
-            <div className='center_piece'>
-                <div className='form'>
-                    Wizard
-                </div>
-            </div>
-        )
+        let { previous, complete, next, setProperty, setDescription, setAddress, setCity, updateState, setZip, setImgurl, setLoan, setMortgage, setRent } = this
+        switch (this.state.wizard) {
+            case 1:
+                return (
+                    <div className='center_piece'>
+                        <div className='form'>
+                            <WizOne next={next} setProperty={setProperty} setDescription={setDescription} />
+                        </div>
+                    </div>
+                )
+            case 2:
+                return (
+                    <div className='center_piece'>
+                        <div className='form'>
+                            <WizTwo next={next} setCity={setCity} setAddress={setAddress} updateState={updateState} setZip={setZip} />
+                        </div>
+                    </div>
+                )
+            case 3:
+                return (
+                    <div className='center_piece'>
+                        <div className='form'>
+                            <WizThree next={next} setImgurl={setImgurl} />
+                        </div>
+                    </div>
+                )
+            case 4:
+                return (
+                    <div className='center_piece'>
+                        <div className='form'>
+                            <WizFour next={next} setLoan={setLoan} setMortgage={setMortgage} />
+                        </div>
+                    </div>
+                )
+            case 5:
+                return (
+                    <div className='center_piece'>
+                        <div className='form'>
+                            <WizFive complete={complete} setRent={setRent} />
+                        </div>
+                    </div>
+                )
+            default:
+                return (
+                    <div className='center_piece'>
+                        <div className='form'>
+                            <WizOne />
+                        </div>
+                    </div>
+                )
+        }
     }
 }
