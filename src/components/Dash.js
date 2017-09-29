d09b967bfd2c8a1bb1375f6ca2fd306ed5240556
Chase Davis <chasewdavis7@gmail.com>
2017-09-29 16:37:55 -0600

    polishing

--- src/components/Dash.js
+++ src/components/Dash.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
 
 export default class Dash extends Component {
     render() {
         return (
             <div className='center_piece'>
                 <div className='form'>
-                    <div>
-                    <button id='add_new'>Add new property</button>
+                    <div className='center'>
+                    <button id='add_new'><Link to='/nav/wizard'>Add new property</Link></button>
                     </div>
                     <div className='filter_row'>
-                        <p>List properties with "desired rent" greator than: $</p><input placeholder='0'></input><button>Filter</button><button>Reset</button>
+                        <p>List properties with "desired rent" greator than: $</p><input placeholder='0'></input><button>Filter</button><button id='dark_green'>Reset</button>
+                    </div>
+                    <div>
+                    <hr size='1' color='black' width='92%'/>
+                    </div>
+                    <div className='listing_div'>
+                        <h5>Home Listings</h5>
+                        <div className='listing'></div>
+                        <div className='listing'></div>
                     </div>
                 </div>
             </div>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,19 +1,53 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
+import axios from 'axios';
+import axiosURL from '../url/url'
 
 export default class Login extends Component {
+
+    constructor(){
+        super()
+        this.state = {
+            username:'',
+            password:''
+        }
+    }
+
+    handleChangeU(value){
+        console.log(value);
+        this.setState({
+            username: value
+        })
+    }
+    handleChangeP(value){
+        console.log(value);
+        this.setState({
+            password: value
+        })
+    }
+
+    login(){
+        axios.post(`${axiosURL}/login`,{ username:this.state.username, password:this.state.password}).then(res=>console.log('getting a response of ' + res.data))
+    }
+
+    register(){
+        axios.post(`${axiosURL}/register`,{ username:this.state.username, password:this.state.password}).then(res=>console.log('getting a response of ' + res.data))
+    }
+
+
+
     render() {
         return (
             <div className='login'>
                 <div className='login_form'>
                     <img src='https://raw.githubusercontent.com/DevMountain/simulation-2/master/assets/auth_logo.png' alt='house'/>
                     <p>Username</p>
-                    <input></input>
+                    <input onChange={(e)=>this.handleChangeU(e.target.value)}></input>
                     <p>Password</p>
-                    <input type='password'></input>
+                    <input onChange={(e)=>this.handleChangeP(e.target.value)} type='password'></input>
                     <div className='login_buttons_div'>
-                    <button><Link className='black' to='/nav/dash'>Login</Link></button>
-                    <button id='dark_green'><Link className='white' to='/nav/dash'>Register</Link></button>
+                    <button onClick={()=>this.login()}><Link className='black' to='/nav/dash'>Login</Link></button>
+                    <button onClick={()=>this.register()} id='dark_green'><Link className='white' to='/nav/dash'>Register</Link></button>
                     </div>
                 </div>
             </div>
