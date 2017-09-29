a67aa07dc3ad6bf269c228bae308ba6ba1a67ac1
Grant Dunn <pointyeyebrows@Grants-MacBook-Pro.local>
2017-09-29 16:24:34 -0600

    wiz4 and 5

--- src/components/wizard/WizFive.js
+++ src/components/wizard/WizFive.js
@@ -2,10 +2,14 @@ import React, { Component } from 'react';
 
 export default class WizFive extends Component {
     render() {
+        let {setRent} = this.props
         return (
             <div className='center_piece'>
                 <div className='form'>
-                    five
+                    <h2> Desired Rent </h2>
+                    <input onChange={e => setRent(e.target.value)}/>
+                    <button> Previous State </button>
+                    <button> Complete </button>
                 </div>
             </div>
         )
--- src/components/wizard/WizFour.js
+++ src/components/wizard/WizFour.js
@@ -2,10 +2,16 @@ import React, { Component } from 'react';
 
 export default class WizFour extends Component {
     render() {
+        let {setAmount, setMortgage, previous, next} = this.props
         return (
             <div className='center_piece'>
                 <div className='form'>
-                    four
+                    <h2> Loan Amount </h2>
+                    <input onChange={e => setAmount(e.target.value)}/>
+                    <h2> Monthly Mortgage </h2>
+                    <input onChange={e => setMortgage(e.target.value)}/>
+                    <button onClick ={next}> Previous Step </button>
+                    <button onClick ={previous}> Next Step </button>
                 </div>
             </div>
         )
